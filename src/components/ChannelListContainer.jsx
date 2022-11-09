5a4b0eb55a922847c10815b8dfc46c952ef8f3c1
Kuper-S <kupkup1992@gmail.com>
2022-11-09 11:12:54 +0200

    add assets folder

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,4 +1,19 @@
 import React from 'react'
+import Cookies from 'universal-cookie'
+import {ChannelList, useChatContext} from "stream-chat-react"
+import {ChannelSeacrch , TeamChannelList, TeamChannelPreviwe} from "./"
+import HospitalIcon from "../assets/icons8-hospital-100.png"
+
+const SideBar = () => (
+    <div className="channel-list__sidebar">
+        <div className='channel-list__sidebar__icon1'>
+            <div className='icon1__inner'>
+                <img src={HospitalIcon} alt="Hospital" width="30"/>
+            </div>
+        </div>
+
+    </div>
+)
 
 function ChannelListContainer() {
   return (
