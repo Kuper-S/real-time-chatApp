bcb27a8bd0ff82f48b59e9f96969435f23f617e8
Kuper-S <kupkup1992@gmail.com>
2022-11-09 08:41:29 +0200

    add css to app and channel comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {StreamChat} from "stream-chat";
+import {Chat} from "stream-chat-react";
+import Cookies from "universal-cookie"
+import {ChannelListContainer,ChannelContainer} from "./components"
+
+// to set chat we need apiKey :
+const apiKey = "2wc9kd8ajn3d";
+
+const client = StreamChat.getInstance(apiKey);
 
 function App() {
   return (
-    <div>
-      <h1>PakaPaka</h1>
+    <div className='app__wrapper'>
+      <Chat client={client} theme="team dark">
+        <ChannelListContainer/>
+        <ChannelContainer/>
+      </Chat>
     </div>
   );
 }
