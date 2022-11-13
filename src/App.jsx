55c1f19ee5447c699e3578f7e493c79497730185
Kuper-S <kupkup1992@gmail.com>
2022-11-13 10:20:09 +0200

    finish Auth Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {StreamChat} from "stream-chat";
 import {Chat} from "stream-chat-react";
 import Cookies from "universal-cookie";
-import {ChannelListContainer,ChannelContainer} from "./components"
+import {ChannelListContainer,ChannelContainer ,Auth} from "./components"
 import "./App.css"
 
 // to set chat we need apiKey :
@@ -10,7 +10,10 @@ const apiKey = "2wc9kd8ajn3d";
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 function App() {
+  if(!authToken) return <Auth/>
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme="team dark">
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+import signinImage from '../assets/signup.jpg';
 
 function Auth() {
+    const [isSingUp, setIsSingUp] = useState(true)
+
+    function handleChange() {
+
+    }
+
+    function switchMode() {
+        setIsSingUp((prevIsSingup) => !prevIsSingup);
+    }
   return (
-    <div>
-      
+    <div className="auth__form-container">
+        <div className="auth__form-container_fields">
+            <div className="auth__form-container_fields-content">
+                <p>{isSingUp ? 'Sing Up' : 'Sing In'}</p>
+                <form onSubmit={""}>
+                    {isSingUp && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='fullName'>
+                                Full Name
+                                <input 
+                                    name='fullName'
+                                    type='text'
+                                    placeholder='Full Name'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                    )}
+                    <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='userName'>
+                                User Name
+                                <input 
+                                    name='username'
+                                    type='text'
+                                    placeholder='User Name'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                    </div>
+                    {isSingUp && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='phoneNumber'>
+                                Phone Number
+                                <input 
+                                    name='phoneNumber'
+                                    type='text'
+                                    placeholder='Phone Number'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                    )}
+                    {isSingUp && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='avatarURL'>
+                                Avatar URL
+                                <input 
+                                    name='AvatarURL'
+                                    type='text'
+                                    placeholder='Avatar URL'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                    )}
+                    <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='password'>
+                                Password
+                                <input 
+                                    name='password'
+                                    type='password'
+                                    placeholder='Password'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                    {isSingUp && (
+                    <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='confirmPassword'>
+                                Confirm Password
+                                <input 
+                                    name='confirmPassword'
+                                    type='password'
+                                    placeholder='Confirm Password'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                        )}
+                        
+                </form>
+                <div className="auth__form-container_fields-account">
+                        <p>
+                            {isSingUp ? "Already have an account ?" : "Dont have an account?"}
+                            <span onClick={switchMode}>
+                                {isSingUp ? 'Sing In' : 'Sing Out'}
+                            </span>
+                        </p>
+                </div>
+            </div>
+        </div>
+        <div className="auth__form-container_image">
+                <img src={signinImage} alt="sign in" />
+            </div>
     </div>
   )
 }
