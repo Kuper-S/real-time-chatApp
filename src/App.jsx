ea59d92a9b70a30b27fed7744bcb6274fa6f3e15
Kuper-S <kupkup1992@gmail.com>
2022-11-15 08:10:26 +0200

    add channel inner snippet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React , {useState} from 'react';
 import {StreamChat} from "stream-chat";
 import {Chat} from "stream-chat-react";
 import Cookies from "universal-cookie";
 import {ChannelListContainer,ChannelContainer ,Auth} from "./components"
+import 'stream-chat-react/dist/css/index.css';
 import "./App.css"
 
 // to set chat we need apiKey :
@@ -27,12 +28,27 @@ if(authToken) {
 }
 
 function App() {
+  const [createType,setCreateType] = useState('')
+  const [isCreating,setIsCreating] = useState(false)
+  const [isEdting,setIsEdting] = useState(false)
+
   if(!authToken) return <Auth/>
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme="team dark">
-        <ChannelListContainer/>
-        <ChannelContainer/>
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating ={ setIsCreating}
+          setCreateType ={setCreateType}
+          setIsEdting = {setIsEdting}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating ={ setIsCreating}
+          isEdting = {isEdting}
+          setIsEdting = {setIsEdting}
+          createType={createType}
+        />
       </Chat>
     </div>
   );
--- src/components/ChannelContainer.jsx
+++ src/components/ChannelContainer.jsx
@@ -4,12 +4,50 @@ import { Channel, useChatContext } from 'stream-chat-react';
 import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './';
 
 
-function ChannelContainer() {
+function ChannelContainer({
+      isCreating,
+      setIsCreating,
+      isEdting,
+      setIsEdting,
+      createType,
+    }) {
   const {channel} = useChatContext();
-  
+
+  if(!isCreating) {
+      return (
+        <div className="channel__container">
+          <CreateChannel
+            createType={createType}
+            setIsCreating = {setIsCreating}
+          />
+
+       </div>
+      )
+  }
+  if(!isEdting) {
+      return (
+        <div className="channel__container">
+            <EditChannel
+            setIsEdting = {setIsEdting}
+          />
+        </div>
+      )
+  }
+  const EmptyState = () =>{
+    <div className='channel-empty__container'>
+          <p className="channel-empty__first">This is the beginning of your chat history.</p>
+          <p className="channel-empty__second">Send messages, attachments, links, emojis, and more!</p>
+    </div>
+  }
+
   return (
-    <div>
-      ChannelContainer
+    <div className=" channel__container">
+      <Channel 
+          EmptyStateIndicator={EmptyState}>
+          Message={(messagesProp , i) => <TeamMessage key={i} {...messagesProp}/>}
+          
+          <ChannelInner setIsEdting={setIsEdting}/>
+      </Channel>
     </div>
   )
 }
